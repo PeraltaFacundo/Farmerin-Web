a36b19b82b41c198313fb25a73552002df256139
Farmerin <infofarmerin@gmail.com>
2024-06-14 12:12:50 -0300

    ,

--- pages/raciones.js
+++ pages/raciones.js
@@ -3,170 +3,120 @@ import axios from 'axios';
 import { Pie } from 'react-chartjs-2';
 import 'chart.js/auto';
 
-
-// URLs de los tambos
-const tambos = [
-  'https://tambo-sanandres-device.dataplicity.io/node/raciones/',
-];
-
-// Nombres de los tambos
-const nombresTambos = [
-'Tambo Farmerin',
-];
-
-function Grafico() {
-const [data, setData] = useState([]);
-const [animalesAusentes, setAnimalesAusentes] = useState([]);
-
-useEffect(() => {
-  async function fetchData() {
-    const allData = await Promise.all(tambos.map(async (url, index) => {
+const Grafico = ({ tamboLink, tamboName }) => {
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get(url);
+        const response = await axios.get(tamboLink);
         const parser = new DOMParser();
         const doc = parser.parseFromString(response.data, 'text/html');
         const table = doc.querySelector('table');
         if (table) {
           const dfUrl = tableToDataFrame(table);
-          return {
-            name: nombresTambos[index] || `Tambo ${index + 1}`,
-            data: dfUrl
-          };
-        } else {
-          return {
-            name: nombresTambos[index] || `Tambo ${index + 1}`,
-            data: null
-          };
+          setData(dfUrl);
         }
       } catch (error) {
         console.error('Error fetching data', error);
-        return {
-          name: nombresTambos[index] || `Tambo ${index + 1}`,
-          data: null
-        };
       }
-    }));
-    setData(allData);
+    };
+    
+    fetchData();
+  }, [tamboLink]);
 
-    // Filtrar y recopilar animales ausentes
-    const ausentes = allData.reduce((acc, tambo) => {
-      if (tambo.data) {
-        const ausentesEnTambo = tambo.data.filter(row => parseInt(row.DiasAusente) >= 2);
-        return acc.concat(ausentesEnTambo);
-      }
-      return acc;
-    }, []);
-    setAnimalesAusentes(ausentes);
-  }
-  fetchData();
-}, []);
-
-return (
-  <div className="containerGrafico">
-    <div className="chartContainer">
-      {data.map((tambo, index) => (
-        <TamboChart key={index} tambo={tambo} />
-      ))}
-    </div>
-    <div className="listContainer">
-      <AnimalesAusentesList animales={animalesAusentes} />
-    </div>
-  </div>
-);
-}
-
-function tableToDataFrame(table) {
-const rows = table.querySelectorAll('tr');
-const headers = Array.from(rows[0].querySelectorAll('th')).map(th => th.textContent.trim());
-return Array.from(rows).slice(1).map(row => {
-  const cells = row.querySelectorAll('td');
-  const obj = {};
-  cells.forEach((cell, i) => {
-    obj[headers[i]] = cell.textContent.trim();
-  });
-  return obj;
-});
-}
-
-function TamboChart({ tambo }) {
-if (!tambo.data) {
   return (
-    <div >
-      <h2>{tambo.name} - Animales En Ordeñe</h2>
-      <img src="../public/FARMERIN_NOTFOUND-transf.png" alt="Not Found" />
+    <div className="contenedor-grafico-formulario">
+      <div className="tambo-chart">
+        <h2>{tamboName} - <span className="titulo-grande">Animales En Ordeñe</span></h2>
+        <ChartDisplay data={data} />
+      </div>
+      <AnimalesAusentesList data={data} />
     </div>
   );
-}
-
-const contarPorNumero = (data, columna, numero) => {
-  return data.filter(row => row[columna] === String(numero)).length;
 };
 
-const contarPorNumeroMayor = (data, columna, numero) => {
-  return data.filter(row => parseInt(row[columna]) >= numero).length;
+const tableToDataFrame = (table) => {
+  const rows = table.querySelectorAll('tr');
+  const headers = Array.from(rows[0].querySelectorAll('th')).map(th => th.textContent);
+  const dfUrl = Array.from(rows).slice(1).map(row => {
+    const cells = row.querySelectorAll('td');
+    const obj = {};
+    cells.forEach((cell, i) => {
+      obj[headers[i]] = cell.textContent;
+    });
+    return obj;
+  });
+  return dfUrl;
 };
 
-const cantidadDiasAusentem1 = contarPorNumero(tambo.data, 'DiasAusente', -1);
-const cantidadDiasAusente0 = contarPorNumero(tambo.data, 'DiasAusente', 0);
-const cantidadDiasAusente1 = contarPorNumero(tambo.data, 'DiasAusente', 1);
-const cantidadDiasAusentemayor = contarPorNumeroMayor(tambo.data, 'DiasAusente', 2);
+const ChartDisplay = ({ data }) => {
+  const contarPorNumero = (data, columna, numero) => {
+    return data.filter(row => row[columna] === String(numero)).length;
+  };
 
-const valores = [cantidadDiasAusentem1, cantidadDiasAusente1, cantidadDiasAusentemayor, cantidadDiasAusente0];
-const nombres = ['NUNCA PASO', 'NO SE LEYO', 'AUSENTE', 'PASANDO'];
-const colores = ['coral', 'red', 'blue', 'green'];
+  const contarPorNumeroMayor = (data, columna, numero) => {
+    return data.filter(row => parseInt(row[columna]) >= numero).length;
+  };
 
-const valoresFiltrados = valores.filter(val => val !== 0);
-const nombresFiltrados = nombres.filter((_, i) => valores[i] !== 0);
-const coloresFiltrados = colores.filter((_, i) => valores[i] !== 0);
+  const cantidadDiasAusentem1 = contarPorNumero(data, 'DiasAusente', -1);
+  const cantidadDiasAusente0 = contarPorNumero(data, 'DiasAusente', 0);
+  const cantidadDiasAusente1 = contarPorNumero(data, 'DiasAusente', 1);
+  const cantidadDiasAusentemayor = contarPorNumeroMayor(data, 'DiasAusente', 2);
 
-const chartData = {
-  labels: nombresFiltrados,
-  datasets: [
-    {
-      data: valoresFiltrados,
-      backgroundColor: coloresFiltrados,
-      borderColor: 'black',
-      borderWidth: 1,
-    },
-  ],
+  const valores = [cantidadDiasAusentem1, cantidadDiasAusente1, cantidadDiasAusentemayor, cantidadDiasAusente0];
+  const nombres = ['Nunca Paso', 'No se leyo', 'Ausente', 'Pasando'];
+  const colores = ['coral', 'red', 'blue', 'green'];
+
+  const valoresFiltrados = valores.filter(val => val !== 0);
+  const nombresFiltrados = nombres.filter((_, i) => valores[i] !== 0);
+  const coloresFiltrados = colores.filter((_, i) => valores[i] !== 0);
+
+  const chartData = {
+    labels: nombresFiltrados,
+    datasets: [
+      {
+        data: valoresFiltrados,
+        backgroundColor: coloresFiltrados,
+        borderColor: 'black',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Pie data={chartData} options={{ maintainAspectRatio: false }} />;
 };
 
-return (
-  <div className="tamboChart">
-    <h2>{tambo.name} - Animales en ordeñe</h2>
-    <Pie data={chartData} />
-  </div>
-);
-}
+const AnimalesAusentesList = ({ data }) => {
+  const animalesAusentes = data.filter(row => parseInt(row.DiasAusente) >= 0);
 
-function AnimalesAusentesList({ animales }) {
-if (animales.length === 0) {
-  return  <div className="loaderGrafico" />;
-}
-
-return (
-  <div className="AnimalesAusentesFormulario">
-    <h2>Lista de Animales Ausentes</h2>
-    <table className="tablaDeAnimales">
-      <thead>
-        <tr>
-          <th>RP</th>
-          <th>eRP</th>
-          <th>Días Ausente</th>
-        </tr>
-      </thead>
-      <tbody>
-        {animales.map((animal, index) => (
-          <tr key={index}>
-            <td>{animal.RP || 'RP desconocido'}</td>
-            <td>{animal.eRP || 'eRP desconocido'}</td>
-            <td>{animal.DiasAusente}</td>
-            
+  return (
+    <div className="formulario">
+      <h3>Animales Ausentes</h3>
+      <table className="tabla-animales">
+        <thead>
+          <tr>
+            <th>RP</th>
+            <th>eRP</th>
+            <th>Días Ausentes</th>
+            <th>Acción</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
-}
+        </thead>
+        <tbody>
+          {animalesAusentes.map((animal, index) => (
+            <tr key={index}>
+              <td>{animal.RP}</td>
+              <td>{animal.eRP}</td>
+              <td>{animal.DiasAusente}</td>
+              <td>
+                <button className="btn-accion" onClick={() => alert(`RP: ${animal.RP}, eRP: ${animal.eRP}`)}>Ver</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 export default Grafico;
